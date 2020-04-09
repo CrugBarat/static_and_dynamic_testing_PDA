8c2456007636def13d6252a56d4bcf51cc6562ce
Craig Barratt <craigtbarratt@gmail.com>
2020-04-09 19:19:42 +0100

    initial commit of PDA static/dynamic testing

---
+++ specs/card_game_specs.rb
@@ -0,0 +1,33 @@
+require("minitest/autorun")
+require('minitest/reporters')
+require_relative('../card.rb')
+require_relative('../card_game.rb')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestCardGame < MiniTest::Test
+
+  def setup()
+    @card1 = Card.new('Club', 3)
+    @card2 = Card.new('Hearts', 14)
+    @cards = [@card1, @card2]
+  end
+
+  def test_check_for_ace__true()
+    assert_equal(true, CardGame.check_for_ace(@card2))
+  end
+
+  def test_check_for_ace__false()
+    assert_equal(false, CardGame.check_for_ace(@card1))
+  end
+
+  def test_highest_card()
+    assert_equal(@card2, CardGame.highest_card(@card1, @card2))
+  end
+
+  def test_cards_total()
+    cards_total = "You have a total of 17"
+    assert_equal(cards_total, CardGame.cards_total(@cards))
+  end
+
+end
